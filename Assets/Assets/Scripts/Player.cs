2f2fe607aa0413af1b448d293338b2c10ef60406
Amaar Ahmed <amaar96@hotmail.co.uk>
2018-10-10 10:59:28 +0100

    Health Bar Project
    Completed Health Bar Tutorial

---
+++ Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stat Health;
+
+    [SerializeField]
+    private Stat energy;
+
+    [SerializeField]
+    private Stat Shield;
+
+    private void Awake()
+    {
+        Health.Initialize();
+        energy.Initialize();
+        Shield.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Health.CurrentVal -= 10;
+        }
+            if (Input.GetKeyDown(KeyCode.W))
+        {
+            Health.CurrentVal += 10;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            energy.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            energy.CurrentVal += 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Shield.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Shield.CurrentVal += 10;
+        }
+
+
+    }
+}
